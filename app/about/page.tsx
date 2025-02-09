50c61508079ac123c9986e238a7423b9ccd05d23
Nicolas Peirano <nicolaspeirano@macbook-air-von-nicolas.home>
2025-02-09 18:01:45 +0100

    Initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold mt-6">Über mich</h1>
+        <p className="mt-4 text-gray-600 max-w-md text-center">
+          Diese App wurde mit Next.js, React und Tailwind CSS entwickelt, um einen einfachen Sparplanrechner bereitzustellen.
+        </p>
+      </div>
+    );
+  }
